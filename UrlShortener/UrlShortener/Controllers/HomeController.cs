ae800bf7d66b19e0d84bdbbee9fbdb665fe89a45
Utkarsh Bajpai <utkrshbjpai.95@gmail.com>
2020-10-10 16:38:09 +0530

    Initial Commit

---
+++ UrlShortener/UrlShortener/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace UrlShortener.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        static List<List<string>> indexes = new List<List<string>>();
+
+        public ActionResult GetUrl(string id) {
+            var element = (from sublist in indexes
+                           where sublist[1].Equals(id)
+                           select sublist).FirstOrDefault();
+
+            id = element[0];
+            id = id.Contains("https://www.") ? id : id.Substring(0, 3) == "www" ? "https://" + id : "https://www." + id;
+            return Redirect(id);
+        }
+
+        public string SetUrl(string inputURL) {
+            var regexItem = new Regex("^[a-zA-Z0-9 ]*$");
+
+            var element = (from sublist in indexes
+                           where sublist[0].Equals(inputURL)
+                           select sublist).FirstOrDefault();
+            if (element == null) {
+
+                var index = "";
+                while (regexItem.IsMatch(index))
+                    index = get_unique_string(6);
+
+                indexes.Add((new List<string> { inputURL, index.Remove(index.Length - 1) }));
+
+                return index.Remove(index.Length - 1);
+            } else
+                return element[1];
+        }
+
+        string get_unique_string(int string_length) {
+            using (var rng = new RNGCryptoServiceProvider()) {
+                var bit_count = (string_length * 6);
+                var byte_count = ((bit_count + 7) / 8); // rounded up
+                var bytes = new byte[byte_count];
+                rng.GetBytes(bytes);
+                return Convert.ToBase64String(bytes);
+            }
+        }
+    }
+}
---
+++ UrlShortener/UrlShortener/Scripts/home.js
@@ -0,0 +1,29 @@
+﻿function submitURL() {
+    var inputUrl = $(".link-creation-input").val();
+    var shortenedURL = "";
+
+    if (validateURL(inputUrl)) {
+
+        $.ajax({
+            type: "POST",
+            url: "/Home/SetUrl",
+            dataType: "text",
+            data: { 'inputURL': inputUrl },
+            async: false,
+            success: function (data) {
+                shortenedURL = window.location.href + data;
+            },
+        });
+
+        $(".origin-link").html(inputUrl);
+        $(".short-link").html('<a href="' + shortenedURL + '" title="' + shortenedURL + '" target="_blank">' + shortenedURL + '</a>');
+        $('.shortened-link').css('display', 'block');
+    }
+    else {
+        alert("Invalid URL")
+    }
+}
+
+function validateURL(value) {
+    return /(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+}
